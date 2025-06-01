3a527f73a39f62f66a93e515856f0d84bcf2c3a5
Dmitry Kostylev <smileyfvce@yandex.ru>
2025-06-02 00:08:19 +0300

    Первый вариант

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/smileyfvce/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция удаления карточки
+  // @todo: Функция создания карточки
 
-// @todo: Вывести карточки на страницу
+function addCard(item, deleteCard) {
+  const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const deleteButton = placesItem.querySelector('.card__delete-button');
+  
+  placesItem.querySelector('.card__title').textContent = item.name;
+  placesItem.querySelector('.card__image').src = item.link;
+  
+  deleteButton.addEventListener('click', () => {
+    deleteCard(placesItem);
+  });
+
+  return placesItem
+};
+
+  // @todo: Функция удаления карточки
+
+function deleteCard(placesItem) {
+  placesItem.remove()
+};
+
+  // @todo: Вывести карточки на страницу
+
+initialCards.forEach( item => {
+  const card = addCard(item, deleteCard);
+  placesList.append(card);
+});
